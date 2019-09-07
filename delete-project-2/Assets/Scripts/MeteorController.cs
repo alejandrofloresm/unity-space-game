22c0bcf8958e6fc6d8cf3093662fa7b22e872d34
Alejandro Flores <alejandro@esporaestudio.com>
2019-09-06 21:53:23 -0500

    Commit inicial

---
+++ delete-project-2/Assets/Scripts/MeteorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorController : MonoBehaviour
+{
+    PlayerController player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject go = GameObject.FindGameObjectWithTag("Player");
+        player = go.GetComponent<PlayerController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        player.Score += 5;
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ delete-project-2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform Bullet;
+    public Transform CannonPosition;
+    public int Score;
+
+    public Text ScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreText.text = "Marcador: " + Score;
+        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        gameObject.transform.Translate(new Vector3(h, v, 0));
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Shoot();
+        }
+    }
+
+    void Shoot() {
+        Instantiate(Bullet, CannonPosition.position, Quaternion.identity);
+    }
+}
